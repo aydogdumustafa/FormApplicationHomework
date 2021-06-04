f91c195825aeae48df1c43b644718a3ecf83063e
aydogdumustafa <69818194+aydogdumustafa@users.noreply.github.com>
2021-06-04 10:29:06 +0200

    Add project files.

---
+++ id04p02/MyMain.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace id04p02
+{
+    partial class MyMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.buttonColour = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonQuit.BackColor = System.Drawing.Color.Red;
+            this.buttonQuit.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.buttonQuit.Location = new System.Drawing.Point(713, 415);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(75, 23);
+            this.buttonQuit.TabIndex = 0;
+            this.buttonQuit.Text = "Quit";
+            this.buttonQuit.UseVisualStyleBackColor = false;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // buttonColour
+            // 
+            this.buttonColour.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonColour.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.buttonColour.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.buttonColour.Location = new System.Drawing.Point(12, 415);
+            this.buttonColour.Name = "buttonColour";
+            this.buttonColour.Size = new System.Drawing.Size(75, 23);
+            this.buttonColour.TabIndex = 1;
+            this.buttonColour.Text = "Colour";
+            this.buttonColour.UseVisualStyleBackColor = false;
+            this.buttonColour.Click += new System.EventHandler(this.buttonColour_Click);
+            // 
+            // MyMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonColour);
+            this.Controls.Add(this.buttonQuit);
+            this.Name = "MyMain";
+            this.Text = "Moje Okienko";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MyMain_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Button buttonColour;
+    }
+}
+
---
+++ id04p02/MyMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace id04p02
+{
+    public partial class MyMain : Form
+    {
+        public MyMain()
+        {
+            InitializeComponent();
+            BackColor = Color.FromArgb(190, 200, 220);
+            Size = new Size(400,400);
+            buttonColour.ForeColor = BackColor;
+            
+        }
+
+        private void MyMain_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            /*
+            if (cnt % 2 == 0)
+            {
+                
+                BackColor = Color.FromArgb(190, 200, 220);
+            }
+            else
+            {
+                BackColor = Color.FromArgb(255, 255, 180);
+            }
+          */
+
+            if (BackColor == Color.FromArgb(255, 255, 180))
+                BackColor = Color.FromArgb(190, 200, 220);
+            else
+                BackColor = Color.FromArgb(255, 255, 180);
+
+                ++cnt;
+            Text = "Moje Okienko   " + cnt;
+        }
+        int cnt = 0;
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            //Close();
+            Application.Exit();
+        }
+
+        private void buttonColour_Click(object sender, EventArgs e)
+        {
+            if (buttonColour.BackColor == BackColor)
+                BackColor = buttonColour.ForeColor;
+
+            Color Temp = BackColor;
+            BackColor = buttonColour.BackColor;
+            buttonColour.BackColor = Temp;
+            buttonColour.ForeColor = BackColor;
+        }
+    }
+}
